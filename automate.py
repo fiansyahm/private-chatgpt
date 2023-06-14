8d3ad791b5303b3daa212dfbdaf44c41a584ed90
fiansyahm <fiansyahmm@gmail.com>
2023-06-14 22:18:48 +0700

    update revisi 1

--- automate.py
+++ automate.py
@@ -26,7 +26,11 @@ def click_selenium(driver,xfullpath,search_by,send_keys=None):
         by_for=By.CSS_SELECTOR
     click_elem = wait.until(EC.element_to_be_clickable((by_for, xfullpath)))
     
-    if send_keys != None:
+    if send_keys == "clear": 
+        click_elem.clear()
+    elif send_keys == "special": 
+        ActionChains(driver).click(click_elem).perform()
+    elif send_keys != None:
         click_elem.send_keys(send_keys)
     else:
         # klik elemen yang sudah terlihat dan dapat diklik
@@ -45,8 +49,8 @@ def get_selenium(driver,xfullpath,search_by):
     click_elem = wait.until(EC.element_to_be_clickable((by_for, xfullpath)))
     return click_elem.text
 
-def run_selenium(excel):
-    isiexcel = pandas.read_csv(excel)
+def run_selenium(csv_file,email,password,timer):
+    isiexcel = pandas.read_csv(csv_file)
     myexcelcontain = isiexcel.values
     # Saving the Excel file
     workbook = Workbook()
@@ -58,48 +62,68 @@ def run_selenium(excel):
     worksheet.cell(row=row, column=col, value="prompt")
     worksheet.cell(row=row, column=col+1, value="output")
     row += 1
+    
+    chromedriver_path = r"chromedriver.exe"  # Update with the actual path
+
+    if os.path.exists(chromedriver_path):
+        print("The file exists")
+    else:
+        print("The file does not exist")
+        chromedriver_path = r"chromedriver.exe"  # Update with the actual path
+
+    # Initialize the WebDriver with the chromedriver path
+    driver = webdriver.Chrome(executable_path=chromedriver_path)
+
+    # Membuka halaman web "https://crewdible.com/seller-app"
+    driver.get("https://ai.undrctrl.id/#/chat")
+
+    driver.maximize_window()
+
+    xfullpath='/html/body/div[1]/div/div[1]/div/div/div/aside/div[1]/div/footer/div/div/div[2]/a/span/div/h2'
+    click_selenium(driver,xfullpath,"xpath")
+        
+    # logins
+    #    email
+    xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/input'
+    click_selenium(driver,xfullpath,"xpath",email)
+
+    #    password
+    xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[2]/div[1]/div/input'
+    click_selenium(driver,xfullpath,"xpath",password)
+
+    time.sleep(10)
+
+    xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[2]/div[1]/div/input'
+    click_selenium(driver,xfullpath,"xpath","\n")
+
 
     for i in range(len(myexcelcontain)):
+        driver.get("https://ai.undrctrl.id/#/chat")
+
+        xfullpath='//*[@id="app"]/div/div[1]/div/div/div/aside/div[1]/div/main/div[2]/div/div[1]/div/div/div/div/div/a/div[2]/button[2]'
+        click_selenium(driver,xfullpath,"xpath")
+
+        time.sleep(5)
+    
+        search_keyword = "Confirm"
+        script = """
+        var buttons = document.getElementsByTagName('button');
+        for (var i = 0; i < buttons.length; i++) {{
+        if (buttons[i].textContent.includes('{0}')) {{
+            buttons[i].click();
+            break;
+        }}
+        }}
+        """.format(search_keyword)
+
+        driver.execute_script(script)
+
+        time.sleep(5)
+
         prompt = myexcelcontain[i][0].replace('\n', ' ')
         topic = myexcelcontain[i][1].replace('\n', ' ')
         print("prompt: ", prompt)
         print("topic: ", topic)
-
-        chromedriver_path = r"chromedriver.exe"  # Update with the actual path
-
-        if os.path.exists(chromedriver_path):
-            print("The file exists")
-        else:
-            print("The file does not exist")
-            chromedriver_path = r"chromedriver.exe"  # Update with the actual path
-
-        # Initialize the WebDriver with the chromedriver path
-        driver = webdriver.Chrome(executable_path=chromedriver_path)
-
-        # Membuka halaman web "https://crewdible.com/seller-app"
-        driver.get("https://ai.undrctrl.id/#/chat")
-
-        driver.maximize_window()
-
-        xfullpath='/html/body/div[1]/div/div[1]/div/div/div/aside/div[1]/div/footer/div/div/div[2]/a/span/div/h2'
-        click_selenium(driver,xfullpath,"xpath")
-        
-    # logins
-    #    email
-        xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/input'
-        click_selenium(driver,xfullpath,"xpath","jl07ko@undrctrl.id")
-
-    #    password
-        xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[2]/div[1]/div/input'
-        click_selenium(driver,xfullpath,"xpath","jl07ko")
-
-        time.sleep(10)
-
-        xfullpath='/html/body/div[2]/div/div/div[1]/div/div[3]/div/div/div[2]/div[2]/div[1]/div/input'
-        click_selenium(driver,xfullpath,"xpath","\n")
-
-        xfullpath='/html/body/div[1]/div/div[1]/div/div/div/aside/div[1]/div/main/div[1]/button'
-        click_selenium(driver,xfullpath,"xpath")
 
         time.sleep(10)
 
@@ -108,17 +132,18 @@ def run_selenium(excel):
 
         click_selenium(driver,xfullpath,"xpath",textarea+"\n")
 
-        time.sleep(30)
+        time.sleep(timer)
         
         xfullpath='/html/body/div[1]/div/div[1]/div/div/div/div/div/div/main/div/div/div/div/div/div[2]/div[2]/div/div[2]/button[2]'
         click_selenium(driver,xfullpath,"xpath")
 
-        xfullpath='/html/body/div[2]/div/div/div[2]'
+        xfullpath='/html/body/div[3]/div/div/div[2]'
         click_selenium(driver,xfullpath,"xpath")
 
         driver.get('https://www.memonotepad.com/')
         time.sleep(2)
         xfullpath='/html/body/div[4]/div/div[2]/div'
+        click_selenium(driver,xfullpath,"xpath",'clear')
         click_selenium(driver,xfullpath,"xpath",(Keys.CONTROL, 'v'))
         time.sleep(2)
         mydata=get_selenium(driver,xfullpath,"xpath")
@@ -128,9 +153,19 @@ def run_selenium(excel):
         worksheet.cell(row=row, column=col+1, value=mydata)
         row += 1
 
-        time.sleep(2)
-        driver.close()
+        folder_path = "output"
 
+        # Specify the file path
+        nomerku=i+1
+        file_path = os.path.join(folder_path, f"hasil_no_{nomerku}.txt")
+
+        # Create the text file
+        with open(file_path, "w") as file:
+            file.write(mydata)
+
+        time.sleep(2)
+        
+    driver.close()
     workbook.save('output.xlsx')
     return workbook
 
@@ -138,7 +173,10 @@ def run_selenium(excel):
 def index():
     if request.method == 'POST':
         csv_file = request.files["csv_file"]
-        workbook=run_selenium(csv_file)
+        email=request.form.get('email')
+        password=request.form.get('password')
+        timer=int(request.form.get('timer'))
+        workbook=run_selenium(csv_file,email,password,timer)
         return render_template('index.html', excel_data=workbook)
     return render_template('index.html')
 
